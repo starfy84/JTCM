cd4d02f50a969ffdb720791f2e6f9eeb3b662ad2
starfy84 <dereck.tu@student.tdsb.on.ca>
2018-05-18 00:31:14 -0400

    Collision

--- main-game-core/src/screens/LevelOne.java
+++ main-game-core/src/screens/LevelOne.java
@@ -4,17 +4,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.Map;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.utils.Disposable;
 import com.main.game.JTCM;
 
-public class LevelOne extends Screen {
+public class LevelOne extends Screen implements ContactListener {
 	BitmapFont font;
-	Texture map, person;
+	Texture map;
+	Sprite person;
 	double xCoord = -2500, yCoord = -700;
+	private int[][] pixels;
 	public LevelOne(ScreenManager sm) {
 		super(sm);
 		map = new Texture("map.png");
-		person = new Texture("person.png");
+		person = new Sprite(new Texture("person.png"));
+		person.setSize(person.getWidth()*2, person.getHeight()*2);
+		person.setPosition(JTCM.WIDTH/2-12, JTCM.HEIGHT/2-29);
 	}
 
 	/**
@@ -24,14 +35,12 @@ public class LevelOne extends Screen {
 	public void getInput() {
 		if (Gdx.input.isKeyPressed(Keys.RIGHT))
 			xCoord -= 3.5;
-		else if (Gdx.input.isKeyPressed(Keys.LEFT))
+		if (Gdx.input.isKeyPressed(Keys.LEFT))
 			xCoord += 3.5;
-		else if (Gdx.input.isKeyPressed(Keys.UP))
+		if (Gdx.input.isKeyPressed(Keys.UP))
 			yCoord -= 3.5;
-		else if (Gdx.input.isKeyPressed(Keys.DOWN))
-		{
+		if (Gdx.input.isKeyPressed(Keys.DOWN))
 			yCoord += 3.5;
-		}
 	}
 
 	@Override
@@ -44,7 +53,7 @@ public class LevelOne extends Screen {
 	public void render(SpriteBatch s) {
 		s.begin();
 		s.draw(map, Math.round(xCoord), Math.round(yCoord), JTCM.WIDTH*5, JTCM.HEIGHT*5);
-		s.draw(person, JTCM.WIDTH/2-90, JTCM.HEIGHT/2-100, person.getWidth()*2, person.getHeight()*2);
+		s.draw(person, person.getX(), person.getY(),person.getWidth(),person.getHeight());
 		s.end();
 
 	}
@@ -52,7 +61,30 @@ public class LevelOne extends Screen {
 	@Override
 	public void dispose() {
 		map.dispose();
-		person.dispose();
+	}
+
+	@Override
+	public void beginContact(Contact contact) {
+
+		
+	}
+
+	@Override
+	public void endContact(Contact contact) {
+
+		
+	}
+
+	@Override
+	public void preSolve(Contact contact, Manifold oldManifold) {
+
+		
+	}
+
+	@Override
+	public void postSolve(Contact contact, ContactImpulse impulse) {
+
+		
 	}
 
 }
