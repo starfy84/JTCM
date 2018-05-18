101ee4068a4da98ff7ab77277bfff81740523573
starfy84 <dereck.tu@student.tdsb.on.ca>
2018-05-17 20:13:23 -0400

    Added images for level select made level classes

---
+++ main-game-core/src/screens/LevelTwo.java
@@ -0,0 +1,36 @@
+package screens;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class LevelTwo extends Screen {
+
+	public LevelTwo(ScreenManager sm) {
+		super(sm);
+
+	}
+
+	@Override
+	public void getInput() {
+
+
+	}
+
+	@Override
+	public void update(double t) {
+
+
+	}
+
+	@Override
+	public void render(SpriteBatch s) {
+
+
+	}
+
+	@Override
+	public void dispose() {
+
+
+	}
+
+}
