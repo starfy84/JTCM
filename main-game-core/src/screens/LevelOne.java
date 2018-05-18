a7a970c39f3bbf64a8b75ed6fd1a9abc8ea3e98a
Faykur <gamevideo2015@gmail.com>
2018-05-17 22:11:47 -0400

    Implemented basic player movement

--- main-game-core/src/screens/LevelOne.java
+++ main-game-core/src/screens/LevelOne.java
@@ -1,41 +1,58 @@
 package screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.main.game.JTCM;
 
 public class LevelOne extends Screen {
 	BitmapFont font;
-
+	Texture map, person;
+	int xCoord = -2500, yCoord = -700;
 	public LevelOne(ScreenManager sm) {
 		super(sm);
-		font = new BitmapFont();
-		font.setColor(Color.BLACK);
-		font.getData().setScale(2);
+		map = new Texture("map.png");
+		person = new Texture("person.png");
 	}
 
+	/**
+	 * This method handles input.
+	 */
 	@Override
 	public void getInput() {
-
+		if (Gdx.input.isKeyJustPressed(Keys.RIGHT))
+			xCoord -= 10;
+		else if (Gdx.input.isKeyJustPressed(Keys.LEFT))
+			xCoord += 10;
+		else if (Gdx.input.isKeyJustPressed(Keys.UP))
+			yCoord -= 10;
+		else if (Gdx.input.isKeyJustPressed(Keys.DOWN))
+		{
+			yCoord += 10;
+		}
 	}
 
 	@Override
 	public void update(double t) {
+		getInput();
 
 	}
 
 	@Override
 	public void render(SpriteBatch s) {
 		s.begin();
-		font.draw(s, "One", 50, 50);
+		s.draw(map, xCoord, yCoord, JTCM.WIDTH*5, JTCM.HEIGHT*5);
+		s.draw(person, JTCM.WIDTH/2-90, JTCM.HEIGHT/2-100, person.getWidth()*5, person.getHeight()*5);
 		s.end();
 
 	}
 
 	@Override
 	public void dispose() {
-		font.dispose();
-
+		map.dispose();
 	}
 
 }
