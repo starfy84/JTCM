9aa9c8b79915acf67fa077ccf8879c1857101983
starfy84 <dereck.tu@student.tdsb.on.ca>
2018-05-18 03:47:59 -0400

    .

--- main-game-core/src/screens/LevelOne.java
+++ main-game-core/src/screens/LevelOne.java
@@ -1,12 +1,18 @@
 package screens;
 
+import org.w3c.dom.css.Rect;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.Map;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
@@ -14,7 +20,7 @@ import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.utils.Disposable;
 import com.main.game.JTCM;
 
-public class LevelOne extends Screen implements ContactListener {
+public class LevelOne extends Screen {
 /**
  * This class runs level one of the game </br>
  * Teacher: Ms. Krasteva </br>
@@ -22,18 +28,24 @@ public class LevelOne extends Screen implements ContactListener {
  * Time spent: 4:00 (including associated graphics) 
  * @author Rohit
  */
-
-	BitmapFont font;
-	Texture map;
-	Sprite person;
-	double xCoord = -2500, yCoord = -700;
-	private int[][] pixels;
+	private Texture map;
+	private Sprite person,detection;
+	private double xCoord = -2500, yCoord = -700;
+	//int[][][]boxes = {{{-437,-1838},{-1746,-1838},{-437,-1640},{-1746,-1640}}};
+	//Rectangle rect = new Rectangle(-437,-1640,1309,198);
+	Rectangle bound;
 	public LevelOne(ScreenManager sm) {
 		super(sm);
 		map = new Texture("map.png");
+//		collide = new Pixmap(Gdx.files.internal("collisionDetection.png"));
 		person = new Sprite(new Texture("person.png"));
 		person.setSize(person.getWidth()*2, person.getHeight()*2);
 		person.setPosition(JTCM.WIDTH/2-12, JTCM.HEIGHT/2-29);
+		detection = new Sprite(new Texture("collisionDetection.png"));
+		detection.setSize(JTCM.WIDTH*5, JTCM.HEIGHT*5);
+		detection.setPosition(Math.round(xCoord), Math.round(yCoord));
+		bound = new Rectangle(-1796,-95,-1732,880);
+		
 	}
 
 	/**
@@ -41,27 +53,40 @@ public class LevelOne extends Screen implements ContactListener {
 	 */
 	@Override
 	public void getInput() {
+
 		if (Gdx.input.isKeyPressed(Keys.RIGHT))
-			xCoord -= 3.5;
+			xCoord -= 5.5;
 		if (Gdx.input.isKeyPressed(Keys.LEFT))
-			xCoord += 3.5;
+			xCoord += 5.5;
 		if (Gdx.input.isKeyPressed(Keys.UP))
-			yCoord -= 3.5;
+			yCoord -= 5.5;
 		if (Gdx.input.isKeyPressed(Keys.DOWN))
-			yCoord += 3.5;
+			yCoord += 5.5;
+		if(Gdx.input.isKeyJustPressed(Keys.SPACE))
+			System.out.println(detection.getX()+" "+detection.getY());
+	
 	}
 
 	@Override
 	public void update(double t) {
 		getInput();
-
+		System.out.println(bound);
+		System.out.println(bound.contains(new Rectangle(detection.getX(),detection.getY(),person.getWidth(),person.getHeight())));
+		System.out.println(detection.getX()+" "+ detection.getY());
+//		System.out.println("Person: "+person.getX()+" "+person.getY());
+//		System.out.println("Detection: "+detection.getX()+" "+detection.getY());
+//		System.out.println(collide.getPixel((int)(detection.getX()-person.getX()), (int)(detection.getY()+person.getY())));
+//		System.out.println((detection.getX()-person.getX())+" "+(detection.getY()-person.getY()));
 	}
 
 	@Override
 	public void render(SpriteBatch s) {
 		s.begin();
+		s.draw(detection, Math.round(xCoord), Math.round(yCoord), JTCM.WIDTH*5, JTCM.HEIGHT*5);
 		s.draw(map, Math.round(xCoord), Math.round(yCoord), JTCM.WIDTH*5, JTCM.HEIGHT*5);
+		detection.setPosition(Math.round(xCoord), Math.round(yCoord));
 		s.draw(person, person.getX(), person.getY(),person.getWidth(),person.getHeight());
+		
 		s.end();
 
 	}
@@ -69,30 +94,6 @@ public class LevelOne extends Screen implements ContactListener {
 	@Override
 	public void dispose() {
 		map.dispose();
-	}
-
-	@Override
-	public void beginContact(Contact contact) {
-
-		
-	}
-
-	@Override
-	public void endContact(Contact contact) {
-
-		
-	}
-
-	@Override
-	public void preSolve(Contact contact, Manifold oldManifold) {
-
-		
-	}
-
-	@Override
-	public void postSolve(Contact contact, ContactImpulse impulse) {
-
-		
 	}
 
 }
