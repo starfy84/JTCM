a7718972f77c13793ffc5d89e494e7bc54e6275c
starfy84 <dereck.tu@hotmail.com>
2018-05-22 11:02:52 -0400

    Update MenuScreen.java

--- main-game-core/src/screens/MenuScreen.java
+++ main-game-core/src/screens/MenuScreen.java
@@ -4,50 +4,40 @@
 package screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.main.game.JTCM;
+import com.badlogic.gdx.math.Vector3;
 
 /**
- * This is the menu screen.</br>
+ * This class contains a blueprint of a screen in our game </br>
  * Teacher: Ms. Krasteva </br>
  * Date: 5/14/18 </br>
- * Time Spent: 15 mins </br>
+ * Time spent: 0:30</br>
  * @version 0.1
  * @author Dereck
  */
-public class MenuScreen extends Screen {
-
-	private Texture menu;
+public abstract class Screen {
+	protected OrthographicCamera cam;
+	protected Vector3 pointer;
+	protected ScreenManager sm;
 
 	/**
-	 * {@link MenuScreen} Constructor
+	 * {@link Screen} Constructor
 	 * 
 	 * @param sm
 	 *            Screen manager.
 	 */
-	public MenuScreen(ScreenManager sm) {
-		super(sm);
-		menu = new Texture("menu.png");
+	public Screen(ScreenManager sm) {
+		this.sm = sm;
+		cam = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		pointer = new Vector3();
 	}
 
 	/**
 	 * This method handles input.
 	 */
-	@Override
-	public void getInput() {
-		if (Gdx.input.isKeyJustPressed(Keys.NUM_1))
-			sm.set(new LevelSelectScreen(sm));
-		else if (Gdx.input.isKeyJustPressed(Keys.NUM_2))
-			sm.set(new HighScoresScreen(sm));
-		else if (Gdx.input.isKeyJustPressed(Keys.NUM_3))
-			sm.set(new InstructionsScreen(sm));
-		else if (Gdx.input.isKeyJustPressed(Keys.NUM_4))
-		{
-			Gdx.app.exit();
-		}
-	}
+	public abstract void getInput();
 
 	/**
 	 * This method will be run on a loop.
@@ -55,11 +45,7 @@ public class MenuScreen extends Screen {
 	 * @param t
 	 *            Delta time.
 	 */
-	@Override
-	public void update(double t) {
-		getInput();
-
-	}
+	public abstract void update(double t);
 
 	/**
 	 * This method draws my graphics
@@ -67,20 +53,10 @@ public class MenuScreen extends Screen {
 	 * @param s
 	 *            The needed sprite batch.
 	 */
-	@Override
-	public void render(SpriteBatch s) {
-		s.begin();
-		s.draw(menu, 0, 0, JTCM.WIDTH, JTCM.HEIGHT);
-		s.end();
-
-	}
+	public abstract void render(SpriteBatch s);
 
 	/**
 	 * This method disposes uneeded resources.
 	 */
-	@Override
-	public void dispose() {
-		menu.dispose();
-	}
-
+	public abstract void dispose();
 }
