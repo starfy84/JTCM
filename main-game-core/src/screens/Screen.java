b104a73ac98e03459d58fd7a1053422e405a14ef
starfy84 <dereck.tu@hotmail.com>
2018-05-28 11:24:42 -0400

    Update Screen.java

--- main-game-core/src/screens/Screen.java
+++ main-game-core/src/screens/Screen.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
-
+import com.badlogic.gdx.assets.AssetManager;
 /**
  * This class contains a blueprint of a screen in our game </br>
  * Teacher: Ms. Krasteva </br>
@@ -19,19 +19,22 @@ public abstract class Screen {
 	protected OrthographicCamera cam;
 	protected Vector3 pointer;
 	protected ScreenManager sm;
-
+        protected AssetManager man;
 	/**
 	 * {@link Screen} Constructor
 	 * 
 	 * @param sm
 	 *            Screen manager.
 	 */
-	public Screen(ScreenManager sm) {
-		this.sm = sm;
-		cam = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		pointer = new Vector3();
-	}
+        public Screen(ScreenManager sm,AssetManager man) {
+                this.sm = sm;
+                this.man = man;
+                cam = new OrthographicCamera(JTCM.WIDTH,JTCM.HEIGHT);
+                cam.translate(JTCM.WIDTH/2-12, JTCM.HEIGHT/2-29);
+                cam.update();
+                //cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+                pointer = new Vector3();
+        }
 
 	/**
 	 * This method handles input.
