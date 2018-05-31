1dac7a30755ca5bce20867e36632e79fc864eb6f
starfy84 <dereck.tu@student.tdsb.on.ca>
2018-05-31 03:00:46 -0400

    Coded read,write, insert methods for highscores

--- main-game-core/src/screens/HighScoresScreen.java
+++ main-game-core/src/screens/HighScoresScreen.java
@@ -2,16 +2,21 @@
  * 
  */
 package screens;
+
+import java.io.*;
+import java.util.*;
+
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
- * This is the main class of our game, it shows a list of highscores. If there arent any highscores, then there will be an error.+
- </br>
+ * This is the main class of our game, it shows a list of highscores. If there
+ * arent any highscores, then there will be an error.+ </br>
  * Teacher: Ms. Krasteva </br>
  * Date: 5/14/18 </br>
+ * 
  * @version 0.1
  * @author Dereck
  */
@@ -25,13 +30,57 @@ public class HighScoresScreen extends Screen {
 	 * @param sm
 	 *            Screen manager.
 	 */
-        public HighScoresScreen(ScreenManager sm, AssetManager man) {
-                super(sm,man);
+	List<Object[]> scores = new ArrayList<Object[]>();
+
+	public HighScoresScreen(ScreenManager sm, AssetManager man) {
+		super(sm, man);
 		font = new BitmapFont();
 		font.setColor(Color.BLACK);
 		font.getData().setScale(2);
 	}
 
+	public void insertScore(Object[] score) {
+		if(scores.size()==0) {
+			scores.add(score);
+			return;
+		}
+		for(int x = scores.size()-1;x>=0;x--)
+			if((Integer)(score[1]) < (Integer)(scores.get(x)[1])) {
+				scores.add(x+1, score);
+				return;
+			}
+		scores.add(0,score);
+	}
+	
+	public void write() {
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("highscores/scores.txt")));
+			for(Object[] x : scores)
+				out.println("{"+x[0]+"}"+" {"+x[1]+"}");
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void read() {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("highscores/scores.txt"));
+			String[] tokens;
+			String inp="";
+			while((inp=in.readLine())!=null) {
+				tokens = inp.split("} {");
+				tokens[0] = tokens[0].substring(1);
+				tokens[1] = tokens[1].substring(0, tokens[1].length()-1);
+				scores.add(new Object[] {tokens[0],Integer.parseInt(tokens[1])});
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	/**
 	 * This method handles input.
 	 */
@@ -60,7 +109,7 @@ public class HighScoresScreen extends Screen {
 	@Override
 	public void render(SpriteBatch s) {
 		s.begin();
-		font.draw(s, "High Scores",50, 50);
+		font.draw(s, "High Scores", 50, 50);
 		s.end();
 
 	}
